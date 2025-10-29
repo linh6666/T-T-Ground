2d592941c9b71627e5ff9b72ecb307e40e7786ac
Quang Linh <quanglink62@gmail.com>
2025-10-29 14:31:24 +0700

    29/10/25lan7

--- components/DeatilSubregion/Menu/index.tsx
+++ components/DeatilSubregion/Menu/index.tsx
@@ -72,7 +72,7 @@ export default function Menu({
 
         data.data.forEach((item: NodeAttributeItem) => {
           const type_vi = (item.model_building_vi as string) || "";
-          if (type_vi.trim() && !uniqueMap.has(type_vi) && type_vi !== "Cảnh quan") {
+          if (type_vi.trim() && !uniqueMap.has(type_vi) && type_vi !== "CẢNH QUAN") {
             uniqueMap.set(type_vi, {
               label: type_vi,
               subzone_vi: subzoneFromQuery,
