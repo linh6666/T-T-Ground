ffdad39c106961a47f559efd287c3dad1e4c2fca
Quang Linh <quanglink62@gmail.com>
2025-11-03 13:49:06 +0700

    3/11/25lan3

--- components/Control1/Menu/index.tsx
+++ components/Control1/Menu/index.tsx
@@ -22,7 +22,7 @@ export default function Menu({ project_id }: MenuProps) {
     { label: "HỆ THỐNG PHÂN KHU", link: `/khu-vuc${project_id ? `?id=${project_id}` : ""}` },
      { label: "HỆ THỐNG TIỆN ÍCH", link: `/tien-ich-1${project_id ? `?id=${project_id}` : ""}` } ,
     { label: "HIỆU ỨNG ÁNH SÁNG", link: `/hieu-ung-anh-sang-1${project_id ? `?id=${project_id}` : ""}` },
-    { label: "THƯ VIỆN", link: "" },
+    { label: "THƯ VIỆN", link:`/Thu-vien-anh-phuoc-tho${project_id ? `?id=${project_id}` : ""}` },
     { label: "MÔ HÌNH", link: "" },
   
   ];
