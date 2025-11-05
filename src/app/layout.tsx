d11dd58943c23bc7195d8693be2c6fa21580dc62
Quang Linh <quanglink62@gmail.com>
2025-11-05 17:20:02 +0700

    5/11/25lan8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,29 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import { MantineProvider } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
-import { ModalsProvider } from "@mantine/modals"; // ✅ thêm dòng này
+import { ModalsProvider } from "@mantine/modals";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import "antd/dist/reset.css";
 import "@mantine/charts/styles.css";
 import AppContainer from "../../common/AppContainer";
 import "./globals.css";
+
+// ✅ Import font Google trực tiếp
+import { Nunito_Sans } from "next/font/google";
+
+const nunito = Nunito_Sans({
+  subsets: ["latin"],
+  weight: [
+    "200",
+    "300",
+    "400",
+    "500",
+   
+  ],
+  style: ["normal", "italic"],
+  variable: "--font-nunito",
+});
 
 export const metadata: Metadata = {
   title: "T&T Group",
@@ -22,11 +38,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={nunito.variable}>
+      <body style={{ fontFamily: "var(--font-nunito), sans-serif" }}>
         <MantineProvider>
           <Notifications position="top-center" />
-          {/* ✅ Bọc toàn bộ nội dung trong ModalsProvider */}
           <ModalsProvider>
             <div
               style={{
@@ -36,9 +51,9 @@ export default function RootLayout({
               }}
             >
               <Header />
-            <main style={{ flex: 1, marginTop: "3%", padding: "2% 0" }}>
-  <AppContainer>{children}</AppContainer>
-</main>
+              <main style={{ flex: 1, marginTop: "3%", padding: "2% 0" }}>
+                <AppContainer>{children}</AppContainer>
+              </main>
               <Footer />
             </div>
           </ModalsProvider>
