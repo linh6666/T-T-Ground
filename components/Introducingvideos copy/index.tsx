01060c83985678cc11b5be7b36e8a5e70fa8c357
Quang Linh <quanglink62@gmail.com>
2025-11-08 14:53:09 +0700

    8/11/25lan4

--- components/Introducingvideos copy/index.tsx
+++ components/Introducingvideos copy/index.tsx
@@ -45,14 +45,15 @@ export default function VideoPage() {
 
   return (
     <Container className={styles.videoContainer}>
-     <iframe
+<iframe
   className={styles.videoIframe}
-  src="https://www.youtube.com/embed/tyVAdYPEDEs?autoplay=1&mute=1"
+  src="https://www.youtube.com/embed/jv0dQMp8phE?autoplay=1&mute=1"
   title="Video giới thiệu"
   frameBorder="0"
   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
   allowFullScreen
 ></iframe>
+
 
       <Button
         className={styles.backButton}
